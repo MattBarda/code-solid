392a8486502c0a02a4e2314e738462b203adb242
mattbarda <mateuszbarda@gmail.com>
2022-06-11 15:17:27 +0200

    Liskov substitution principle.

--- src/Scoring/ScoringFactorInterface.php
+++ src/Scoring/ScoringFactorInterface.php
@@ -6,5 +6,10 @@ use App\Entity\BigFootSighting;
 
 interface ScoringFactorInterface
 {
+    /**
+     * Return the score that should be added to overall score.
+     *
+     * This method should not return exception for any normal reason.
+     */
     public function score(BigFootSighting $sighting): int;
 }
---
+++ src/Service/DebuggableSightingScorer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\BigFootSighting;
+use App\Model\DebuggableBigFootSightingScore;
+
+class DebuggableSightingScorer extends SightingScorer
+{
+    public function score(BigFootSighting $sighting): DebuggableBigFootSightingScore
+    {
+        $bfScore = parent::score($sighting);
+        return new DebuggableBigFootSightingScore(
+            $bfScore->getScore(),
+            100
+        );
+    }
+}
